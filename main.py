de321994f578202add7cb5cd768d904d90b65d28
matrecole <144698344+matrecole@users.noreply.github.com>
2023-10-12 21:31:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-import random, os, time  # os pour utiliser la commande cls (Clear Screen) et time pour faire des pauses
+import os  # os pour utiliser la commande cls (Clear Screen)
+import random  # pour avoir des nombres au hazard
+import time  # time pour faire des pauses
+
+
+def essais():
+    essai = int(input("Entrez votre essai: "))
+    os.system("cls")
+    if essai == nombre:
+        devine = True
+
+    elif essai < nombre:
+        print("Le nombre auquel je pense est plus grand que votre essai")
+        time.sleep(3)
+        os.system("cls")
+
+        devine = False
+
+    elif essai > nombre:
+        print("Le nombre auquel je pense est plus petit que votre essai")
+        time.sleep(3)
+        os.system("cls")
+
+        devine = False
+
+    return devine  # savoir si le nombre a été deviné ou non
+
 
 game = True  # sert a dire au jeu si on continue ou non
 
 while game:
-    devine = False  # savoir si le nombre a été deviné ou non
+
     nb_essai = 0
     min = int(input("Entrez le minimum pour que je choisisse un nombre: "))
     os.system("cls")  # efface le terminal
@@ -16,23 +42,10 @@ while game:
     time.sleep(5)  # pause de 5 secondes
     os.system("cls")
 
-    while not devine:  # boucle tant que le nombre n'a pas été deviné
-        essai = int(input("Entrez votre essai: "))
-        os.system("cls")
-        if essai == nombre:
-            devine = True
-
-        elif essai < nombre:
-            print("Le nombre auquel je pense est plus grand que votre essai")
-            time.sleep(3)
-            os.system("cls")
-
-        elif essai > nombre:
-            print("Le nombre auquel je pense est plus petit que votre essai")
-            time.sleep(3)
-            os.system("cls")
-
+    while True:
         nb_essai += 1  # +1 essai
+        if essais():
+            break
 
     print(f"Bravo ! Bonne réponse\nVous avez réussi en {nb_essai} essais.")
 
@@ -43,4 +56,3 @@ while game:
 
     else:  # fait quitter le joueur si il choisis de quitter ou s'il n'écrit pas y ou n
         game = False
-        
